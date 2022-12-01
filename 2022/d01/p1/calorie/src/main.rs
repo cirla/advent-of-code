7e8f1db5aed112dae37dea6b6e857f1230c129a6
Tim Cheeseman <tim@timcheeseman.com>
2022-12-01 12:36:00 -0500

    2022 d1

---
+++ 2022/d01/p1/calorie/src/main.rs
@@ -0,0 +1,31 @@
+use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead};
+
+use itertools::Itertools;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let input = File::open(&args[1])?;
+    let reader = io::BufReader::new(input);
+
+    let highest = &reader
+        .lines()
+        .filter_map(|line| line.ok())
+        .group_by(|x| !x.is_empty())
+        .into_iter()
+        .filter_map(|(key, group)| {
+            if key {
+                Some(group.filter_map(|x| x.parse::<u32>().ok()).sum::<u32>())
+            } else {
+                None
+            }
+        })
+        .max()
+        .unwrap();
+
+    println!("{}", highest);
+
+    Ok(())
+}
---
+++ 2022/d01/p1/run.sh
@@ -0,0 +1,1 @@
+cd calorie && cargo run -- ../../input.txt
